008011ca67028fd343b4c2cf91e38d9161d99167
Zak Strassberg <zakstrassberg@gmail.com>
2017-10-22 03:02:21 -0400

    tracker container

--- src/containers/Tracker/index.js
+++ src/containers/Tracker/index.js
@@ -1,14 +1,33 @@
 import { connect } from 'react-redux';
-import { map } from 'lodash';
+import { map, orderBy, filter, first } from 'lodash';
 
-import { updateCombatantAction } from '../../store/tracker/actions';
+import {
+  endTurnAction,
+  sortCombatantsAction,
+  updateCombatantAction,
+} from '../../store/tracker/actions';
 import Tracker from '../../components/pages/Tracker';
 
-const mapStateToProps = ({ tracker: { combatantsById, allCombatants } }) => ({
-  combatants: map(allCombatants, id => combatantsById[id]),
-});
+const mapStateToProps = ({ tracker: { combatantsById, allCombatants, turn } }) => {
+  const sortedCombatants = orderBy(combatantsById, 'initiative', 'desc');
+  const noTurnOverSortedCombatants = filter(sortedCombatants, { turnOver: false });
+  const activeCombatants = filter(noTurnOverSortedCombatants, {
+    initiative: (first(noTurnOverSortedCombatants) || {}).initiative,
+  });
+
+  return {
+    combatants: map(allCombatants, id => combatantsById[id]),
+    sortedCombatants,
+    turn,
+    activeCombatants,
+  };
+};
 const mapDispatchToProps = dispatch => ({
   dispatchUpdateCombatant: newCombatant => dispatch(updateCombatantAction(newCombatant)),
+  dispatchEndTurnActions: (combatantIdsSortedByInitiative) => {
+    dispatch(endTurnAction());
+    dispatch(sortCombatantsAction(combatantIdsSortedByInitiative));
+  },
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Tracker);
