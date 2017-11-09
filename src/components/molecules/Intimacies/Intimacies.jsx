f3b83ee4a1fea99bec424e415c0130bc0182144a
Zak Strassberg <zakstrassberg@gmail.com>
2017-11-08 23:35:53 -0500

    worked on intimacies

--- src/components/molecules/Intimacies/Intimacies.jsx
+++ src/components/molecules/Intimacies/Intimacies.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import { every, get } from 'lodash';
 
 import { Caption } from '../../atoms/Typography';
 import Input from '../../atoms/Input';
-import {StyledRadiogroup} from '../../atoms/Radiogroup';
+import { StyledRadiogroup } from '../../atoms/Radiogroup';
 
 const IntimaciesWrapper = styled.section``;
 const IntimaciesList = styled.ul``;
@@ -14,15 +15,20 @@ export default class Intimacies extends Component {
   static propTypes = {
     intimacies: PropTypes.arrayOf(
       PropTypes.shape({
+        id: PropTypes.number.isRequired,
         level: PropTypes.oneOf(['MINOR', 'MAJOR', 'DEFINING']),
         type: PropTypes.oneOf(['TIE', 'PRINCIPLE']),
         description: PropTypes.string,
       })
-    ),
+    ).isRequired,
+    createIntimacy: PropTypes.func.isRequired,
+    updateIntimacy: PropTypes.func.isRequired,
+    destroyIntimacy: PropTypes.func.isRequired,
   };
   static defaultProps = {
     intimacies: [
       {
+        id: 1,
         level: 'DEFINING',
         type: 'TIE',
         description: 'Cynis Nadol (Brotherly Love)',
@@ -34,15 +40,40 @@ export default class Intimacies extends Component {
     super(props);
 
     this.state = {
-      newLevel: null,
-      newType: null,
-      newDescription: '',
+      newIntimacy: {
+        level: null,
+        type: null,
+        description: '',
+      },
     };
   }
 
-  renderIntimacy = ({ level, type, description }) => (
+  editNewIntimacy = field => (e) => {
+    const value = get(e, 'target.value', e);
+    this.setState({ newIntimacy: { ...this.state.newIntimacy, [field]: value } });
+  };
+
+  newIntimacyValid = () => every(this.state.newIntimacy);
+
+  handleClickNewIntimacy = (e) => {
+    e.preventDefault();
+    if (this.newIntimacyValid()) {
+      this.props.createIntimacy(this.state.newIntimacy);
+    } else {
+      console.warn('Intimacy Invalid:', this.state.newIntimacy);
+    }
+  };
+
+  renderIntimacy = ({ id, level, type, description }) => (
     <Intimacy>
       {level} {type} - {description}
+      <button
+        onClick={() => {
+          this.props.destroyIntimacy(id);
+        }}
+      >
+        x
+      </button>
     </Intimacy>
   );
 
@@ -57,18 +88,25 @@ export default class Intimacies extends Component {
           items={['MINOR', 'MAJOR', 'DEFINING'].map(label => ({
             label,
             value: label,
-            checked: this.state.newLevel === label,
+            checked: this.state.newIntimacy.level === label,
           }))}
+          onClick={this.editNewIntimacy('level')}
         />
         <StyledRadiogroup
           name="newType"
           items={['PRINCIPLE', 'TIE'].map(label => ({
             label,
             value: label,
-            checked: this.state.newType === label,
+            checked: this.state.newIntimacy.type === label,
           }))}
+          onClick={this.editNewIntimacy('type')}
         />
-        <Input name="newDescription" value={this.state.newDescription} />
+        <Input
+          name="newDescription"
+          value={this.state.newIntimacy.description}
+          onChange={this.editNewIntimacy('description')}
+        />
+        <Input type="submit" value="Add Intimacy" onClick={this.handleClickNewIntimacy} />
       </IntimaciesWrapper>
     );
   }
